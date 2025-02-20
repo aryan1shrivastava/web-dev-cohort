e546c35ef14abb446224fb002595109527d09b52
Aryan Shrivastava <aryan1shrivastava@gmail.com>
2025-02-20 18:58:55 +0530

    updated class.js

--- fundamentals/js/class.js
+++ fundamentals/js/class.js
@@ -11,6 +11,8 @@ class Person {
 
 const p1 = new Person("Aryan", "Shrivastava");
 const p2 = new Person("Jethalal", "Gada");
+//this just takes the prototype of the class and puts it into the __proto__ of p1(object)
+//and then we can access the getFullname method from the prototype of the class
 
 console.log(p1.getFullname());
 console.log(p2.getFullname());
